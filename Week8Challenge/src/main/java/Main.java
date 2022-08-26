064ef4dfd563bdff2acbbe422aa1013a7bca8afc
Jessi Foley <jessifreyberger@gmail.com>
2022-08-26 12:45:30 -0500

    Changes to get main working

--- Week8Challenge/src/main/java/Main.java
+++ Week8Challenge/src/main/java/Main.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         List<String> input = Collections.emptyList();
@@ -23,7 +30,7 @@ public class Main {
         return fileInput;
     }
 
-    public static void writeFile(ArrayList<Track> output) {
+    public static void writeFile(List<Track> output) {
 
     }
 }
